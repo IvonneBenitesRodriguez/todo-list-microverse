68ce36acccb68559a30ead626ce4e95ca54bced0
Ivonne <ivonnebenites2015@gmail.com>
2023-09-01 19:27:38 -0500

    Chore: fix error found by ESLinttool

--- src/js/list.js
+++ src/js/list.js
@@ -1,23 +1,23 @@
 class List {
-    constructor() {
-      this.localStorage = localStorage;
-    }
-  
+  constructor() {
+    this.localStorage = localStorage;
+  }
+
       getLocalStorage = () => {
         const savedlistData = this.localStorage.getItem('lists-data');
         const data = savedlistData ? JSON.parse(savedlistData) : [];
         return data;
       };
-  
+
       setLocalStorage = (data) => {
         this.localStorage.setItem('lists-data', JSON.stringify(data));
       };
-  
+
       getTaskByIndex = (id) => {
         const data = this.getLocalStorage().filter((e) => e.index === Number(id));
         return data;
       };
-  
+
       UpdateCompleteTask = (index) => {
         const data = this.getLocalStorage();
         data.forEach((e) => {
@@ -27,7 +27,7 @@ class List {
         });
         this.setLocalStorage(data);
       };
-  
+
       UpdateUnCompleteTask = (index) => {
         const data = this.getLocalStorage();
         data.forEach((e) => {
@@ -37,7 +37,7 @@ class List {
         });
         this.setLocalStorage(data);
       };
-  
+
       UpdateDescriptionTask = (index, value) => {
         const data = this.getLocalStorage();
         data.forEach((e) => {
@@ -47,7 +47,7 @@ class List {
         });
         this.setLocalStorage(data);
       };
-  
+
       removeLocalStorageListData = (index) => {
         const data = this.getLocalStorage().filter((e) => e.index !== index);
         data.forEach((e, i) => {
@@ -55,7 +55,7 @@ class List {
         });
         return data;
       };
-  
+
       removeAllCompletedTask = () => {
         const data = this.getLocalStorage().filter((e) => !e.completed);
         data.forEach((e, i) => {
@@ -64,22 +64,22 @@ class List {
         this.setLocalStorage(data);
         return data;
       };
-  
+
       UpdatedIndexWhenDragItem = (index, index2) => {
         const data = this.getLocalStorage();
         const index1Data = data.find((e) => e.index === Number(index));
         const index2Data = data.find((e) => e.index === Number(index2));
-  
+
         if (index1Data && index2Data) {
           const tempIndex = index1Data.index;
           index1Data.index = index2Data.index;
           index2Data.index = tempIndex;
         }
-  
+
         this.setLocalStorage(data);
         return data;
       };
-  
+
       addTaskToList = (listDetails) => {
         const data = this.getLocalStorage();
         listDetails.index = data.length + 1;
@@ -87,12 +87,12 @@ class List {
         this.setLocalStorage(data);
         return listDetails;
       };
-  
+
       removeTaskFromList = (index) => {
         const data = this.removeLocalStorageListData(index);
         this.setLocalStorage(data);
         return data;
       };
-  }
-  
-  export default List;
+}
+
+export default List;
