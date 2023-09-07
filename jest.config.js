80d66b7476bff41fb226e1191ec385fe09652322
Ivonne <ivonnebenites2015@gmail.com>
2023-09-07 10:20:57 -0500

    Remove unused code

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//     testEnvironment: 'jsdom',
-//     "testMatch": ["**/__tests__/**/*.test.js"],
-//     setupFilesAfterEnv: ['./testSetup.js'],
-//     transform: {
-//       '^.+\\.jsx?$': 'babel-jest',
-//     },
-//     moduleNameMapper: {
-//       '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/__mocks__/imageMock.js',
-//       '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-//     },
-//   };
-
 module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.js'], // Matches all files ending with .test.js
