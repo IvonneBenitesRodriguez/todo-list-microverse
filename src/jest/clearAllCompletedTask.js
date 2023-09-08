36500a1174a9607fdfcc338a8f9e4fcd72d730b7
Ivonne <ivonnebenites2015@gmail.com>
2023-09-07 19:40:47 -0500

    Update: fix error found by linters

--- src/jest/clearAllCompletedTask.js
+++ src/jest/clearAllCompletedTask.js
@@ -1,34 +1,26 @@
-import List from './list.js';
+import List from '../js/list.js';
 
-// Define the clearAllCompletedTask function
-const clearAllCompletedTask = () => {
-  // Create a new instance of the List class
-  const list = new List();
-
-  // Get the clear button element
-  const btnClear = document.querySelector('.btn-clear');
-
-  // Add a click event listener to the clear button
-  btnClear.addEventListener('click', () => {
-    // Get all elements with the 'completed' class
-    const completed = document.querySelectorAll('.completed');
-
-    // Remove completed tasks from the list and get the updated data
-    const data = list.removeAllCompletedTask();
-
-    // Remove the completed task elements from the DOM
-    completed.forEach((item) => {
-      const parent = item.parentNode.parentNode;
-      parent.remove();
-    });
-
-    // Update the 'index' attribute of task buttons
-    const btns = document.querySelectorAll('.btn-task');
-    btns.forEach((btn, i) => {
-      btn.setAttribute('index', data[i].index);
-    });
+// Function to handle the logic of removing completed tasks and updating attributes
+export const handleClearButtonClick = (list) => {
+  const completed = document.querySelectorAll('.completed');
+  const data = list.removeAllCompletedTask();
+  completed.forEach((item) => {
+    const parent = item.parentNode.parentNode;
+    parent.remove();
+  });
+  const btns = document.querySelectorAll('.btn-task');
+  btns.forEach((btn, i) => {
+    btn.setAttribute('index', data[i].index);
   });
 };
 
-// Export the clearAllCompletedTask function
-module.exports = clearAllCompletedTask
+// Function to add a click event listener to the clear button
+export const addClearButtonEventListener = () => {
+  const btnClear = document.querySelector('.btn-clear');
+  if (btnClear) {
+    btnClear.addEventListener('click', () => {
+      const list = new List();
+      handleClearButtonClick(list);
+    });
+  }
+};
