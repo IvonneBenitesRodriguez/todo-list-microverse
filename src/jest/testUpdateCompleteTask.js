11003d0d0a48f5069476389d9d260cec46e2654f
Ivonne <ivonnebenites2015@gmail.com>
2023-09-08 16:28:11 -0500

    Add testUpdateCompleteTask.js file

--- src/jest/testUpdateCompleteTask.js
+++ src/jest/testUpdateCompleteTask.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import List from '../js/List';
 
 describe('List', () => {
@@ -54,3 +55,27 @@ describe('List', () => {
     // Clean up if necessary
   });
 });
+=======
+import List from '../js/list.js';
+
+// Create an instance of the List class
+const myList = new List();
+
+// Function to test the UpdateCompleteTask function
+export function testUpdateCompleteTask() {
+    // Choose an index to test (replace with a valid index from your data)
+    const indexToUpdate = 1;
+
+    // Call the UpdateCompleteTask function
+    myList.UpdateCompleteTask(indexToUpdate);
+}
+
+// Function to test the UpdateUnCompleteTask function
+export function testUpdateUnCompleteTask() {
+    // Choose an index to test (replace with a valid index from your data)
+    const indexToUpdate = 1;
+
+    // Call the UpdateUnCompleteTask function
+    myList.UpdateUnCompleteTask(indexToUpdate);
+}
+>>>>>>> fceceec8 (Add testUpdateCompleteTask.js)
