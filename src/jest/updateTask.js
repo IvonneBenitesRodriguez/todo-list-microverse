2eed2422f6ac06eeb09a6eff1a78c657f6c813eb
MarkYegon7 <markyegonk@gmail.com>
2023-09-08 22:43:30 +0300

    Add updateTask.js file

---
+++ src/jest/updateTask.js
@@ -0,0 +1,56 @@
+import List from './List';
+
+describe('List', () => {
+  let list;
+
+  beforeAll(() => {
+    list = new List();
+  });
+
+  afterEach(() => {
+    // Reset or clean up any test-specific state if necessary
+  });
+
+  it('should update a task to completed', () => {
+    // Arrange: Set up a test list with some tasks and data in localStorage
+    const initialData = [
+      { index: 1, description: 'Task 1', completed: false },
+      { index: 2, description: 'Task 2', completed: false },
+    ];
+    list.setLocalStorage(initialData);
+
+    // Act: Call the update method
+    list.UpdateCompleteTask(1);
+
+    // Assert: Verify that the task was updated
+    const updatedData = list.getLocalStorage();
+    expect(updatedData.find((task) => task.index === 1).completed).toBe(true);
+  });
+
+  it('should update a task to uncompleted', () => {
+    // Similar to the previous test, but test for UpdateUnCompleteTask
+  });
+
+  it('should update a task description', () => {
+    // Arrange: Set up a test list with some tasks and data in localStorage
+    const initialData = [
+      { index: 1, description: 'Task 1', completed: false },
+      { index: 2, description: 'Task 2', completed: false },
+    ];
+    list.setLocalStorage(initialData);
+
+    // Act: Call the update method
+    const newDescription = 'Updated Task 1 Description';
+    list.UpdateDescriptionTask(1, newDescription);
+
+    // Assert: Verify that the task description was updated
+    const updatedData = list.getLocalStorage();
+    expect(updatedData.find((task) => task.index === 1).description).toBe(newDescription);
+  });
+
+  // Write more tests for other methods if needed
+
+  afterAll(() => {
+    // Clean up if necessary
+  });
+});
