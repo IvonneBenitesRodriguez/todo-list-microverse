82134fc355fc48e0a987aaae706469c0f20f5dcb
Ivonne <ivonnebenites2015@gmail.com>
2023-09-08 20:02:46 -0500

    Chore: update testUpdateCompleteTask.js

--- src/jest/testUpdateCompleteTask.js
+++ src/jest/testUpdateCompleteTask.js
@@ -5,18 +5,18 @@ const myList = new List();
 
 // Function to test the UpdateCompleteTask function
 export function testUpdateCompleteTask() {
-    // Choose an index to test (replace with a valid index from your data)
-    const indexToUpdate = 1;
+  // Choose an index to test (replace with a valid index from your data)
+  const indexToUpdate = 1;
 
-    // Call the UpdateCompleteTask function
-    myList.UpdateCompleteTask(indexToUpdate);
+  // Call the UpdateCompleteTask function
+  myList.UpdateCompleteTask(indexToUpdate);
 }
 
 // Function to test the UpdateUnCompleteTask function
 export function testUpdateUnCompleteTask() {
-    // Choose an index to test (replace with a valid index from your data)
-    const indexToUpdate = 1;
+  // Choose an index to test (replace with a valid index from your data)
+  const indexToUpdate = 1;
 
-    // Call the UpdateUnCompleteTask function
-    myList.UpdateUnCompleteTask(indexToUpdate);
+  // Call the UpdateUnCompleteTask function
+  myList.UpdateUnCompleteTask(indexToUpdate);
 }
